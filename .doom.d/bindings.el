ede85ff9a141b8faeabe23d8a88dc4a377355ae6
Heikki Lappalainen <heikki.lappalainen@protonmail.com>
2020-06-22 17:44:40 +0300

    Added 'peep-dired' image preview

--- .doom.d/bindings.el
+++ .doom.d/bindings.el
@@ -1,5 +1,13 @@
 ;;; ~/.doom.d/bindings.el -*- lexical-binding: t; -*-
 
 ;; Comment line
-;; (map! :leader "c l" #'comment-line)
 (map! "C-/" #'comment-line)
+
+;; Dired image preview
+(after! dired-jump
+  (map! :localleader
+        :nv "i" #'peep-dired))
+(evil-define-key 'normal peep-dired-mode-map
+  (kbd "j") 'peep-dired-next-file
+  (kbd "k") 'peep-dired-prev-file)
+(add-hook 'peep-dired-hook 'evil-normalize-keymaps)
