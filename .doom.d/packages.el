2e05ec671947e4846744da1e71ea73ecdd660d45
Heikki Lappalainen <heikki.lappalainen@protonmail.com>
2020-06-28 23:02:27 +0300

    Added exwm (use Emacs as a window manager)

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -49,4 +49,8 @@
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
 
+;; Show image preview in dired
 (package! peep-dired)
+
+;; Use Emacs as a window manager
+(package! exwm)
