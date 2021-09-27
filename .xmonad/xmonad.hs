98315647781d55ca413da6d88eed83f4daf168fb
Heikki Lappalainen <heikki.lappalainen@protonmail.com>
2021-09-27 09:35:25 +0300

    Update

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -114,7 +114,7 @@ myModMask :: KeyMask
 myModMask = mod4Mask -- Sets modkey to super/windows key
 
 myTerminal :: String
-myTerminal = "alacritty" -- Sets default terminal
+myTerminal = "kitty" -- Sets default terminal
 
 myBrowser :: String
 myBrowser = "brave"
